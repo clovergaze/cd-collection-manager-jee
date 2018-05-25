42315ad05adddf563cde86dad4b4c69bd3b3ee16
Johannes Hillert <johannes.hillert@infokin.org>
2018-05-25 17:56:59 +0200

    Corrected edit functionality

--- src/main/java/org/infokin/cdcollectionmanager/bean/AlbumBean.java
+++ src/main/java/org/infokin/cdcollectionmanager/bean/AlbumBean.java
@@ -4,14 +4,15 @@ import org.infokin.cdcollectionmanager.model.Album;
 import org.infokin.cdcollectionmanager.model.Band;
 import org.infokin.cdcollectionmanager.service.AlbumService;
 
-import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
 
 @Named
-@RequestScoped
-public class AlbumBean {
+@ViewScoped
+public class AlbumBean implements Serializable {
 
     /*----------
     | Services |
--- src/main/java/org/infokin/cdcollectionmanager/bean/BandBean.java
+++ src/main/java/org/infokin/cdcollectionmanager/bean/BandBean.java
@@ -3,14 +3,15 @@ package org.infokin.cdcollectionmanager.bean;
 import org.infokin.cdcollectionmanager.model.Band;
 import org.infokin.cdcollectionmanager.service.BandService;
 
-import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
 
 @Named
-@RequestScoped
-public class BandBean {
+@ViewScoped
+public class BandBean implements Serializable {
 
     /*----------
     | Services |
